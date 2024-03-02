67fe91a61435a99e945f3f53abfadbaab54e573a
Promise Mwenga <promise_mwenga@hotmail.com>
2024-03-01 22:51:07 -0400

    edit to teams and athlete models, reduced the relationships with user

--- flask_package/models.py
+++ flask_package/models.py
@@ -10,7 +10,7 @@ def load_user(user_id):
 class User(UserMixin, db.Model):
     """User account model."""
 
-    __tablname__ = "users"
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     first_name: str = db.Column(db.String(16), index=True, unique=False)
@@ -22,11 +22,10 @@ class User(UserMixin, db.Model):
     nccp_number: int = db.Column(db.Integer, index=True, unique=True)
     nbiaa_policy: str = db.Column(db.String) 
     joined = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
-    username = db.Column(db.String(140), index=True, unique=True)
-    password_hash = db.Column(db.String(140))
+    username: str = db.Column(db.String(140), index=True, unique=True)
+    password_hash: str = db.Column(db.String(140))
     
-    teams = db.relationship('Team', backref='user', lazy='dynamic')
-    athletes = db.relationship('Athlete', backref='user', lazy='dynamic')
+    teams = db.relationship('Team', backref='users', lazy='dynamic')
     
     def __repr__(self):
         return f"User(Name: {self.first_name} {self.last_name}, role: {self.role})"
@@ -36,36 +35,41 @@ class User(UserMixin, db.Model):
 
     def check_password(self, password: str):
         return check_password_hash(self.password_hash, password)
-    
-    def is_password_correct(self, password: str):
-        return check_password_hash(self.password_hash, password)
-    
-    @staticmethod
-    def _generate_password_hash(password):
-        return generate_password_hash(password)
 
 class Team(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    team_name = db.Column(db.String(140), index=True, unique=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    """Team model."""
 
-    athletes = db.relationship('Athlete', backref='team', lazy='dynamic')
+    __tablename__ = 'teams'
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    team_name: str = db.Column(db.String(140), index=True, unique=True)
+
+    user_id: int = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    athletes = db.relationship('Athlete', backref='teams', lazy='dynamic')
 
     def __repr__(self):
         return f"Team: {self.team_name}"
 
 class Athlete(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, index=True, unique=False)
-    email = db.Column(db.String(140), index=True, unique=True)
-    date_of_birth = db.Column(db.String, index=True, unique=False)
-    student_id = db.Column(db.Integer, index=True, unique=True)
-    position = db.Column(db.Integer, index=True, unique=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    team_id = db.Column(db.Integer, db.ForeignKey('team.id'))
+    """Athlete model."""
+
+    __tablename__ = 'athletes'
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    first_name: str = db.Column(db.String(16), index=True, unique=False)
+    last_name: str = db.Column(db.String(16), index=True, unique=False)
+    email: str = db.Column(db.String(60), index=True, unique=True)
+    date_of_birth: str = db.Column(db.String(16), index=True, unique=False)
+    student_id: int = db.Column(db.Integer, index=True, unique=True)
+    year_started_grade_9: int = db.Column(db.Integer)
+    current_grade: int = db.Column(db.Integer)
+    position: int = db.Column(db.Integer, index=True, unique=False)
+    
+    team_id: int = db.Column(db.Integer, db.ForeignKey('teams.id'))
 
     def __repr__(self):
-        return f"Athlete Name: {self.name}"
+        return f"Athlete(Student Id: {self.student_id}, Name: {self.first_name} {self.last_name}, Email: {self.email})"
 
 
 class Staff(db.Model):
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,7 +1,7 @@
 """
 This file (test_models.py) contains the unit tests for the models.py file.
 """
-from flask_package.models import User
+from flask_package.models import User, Athlete, Team
 from werkzeug.security import generate_password_hash
 
 def test_new_user():
@@ -11,9 +11,34 @@ def test_new_user():
     THEN check the first name, last name , username, hashed_passowrd, and role fields are defined correctly
     """
 
-    user = User(first_name='Promise', last_name='Mwenga', role='coach', username='test1', password_hash=generate_password_hash('test1', method='sha256'))
+    user = User(id=1, first_name='Promise', last_name='Mwenga', role='coach', username='test1', password_hash=generate_password_hash('test1'))
     assert user.first_name == 'Promise'
     assert user.last_name == 'Mwenga'
     assert user.username == 'test1'
     assert user.password_hash != 'test1'
     assert user.role == 'coach'
+
+
+def test_new_team():
+    """
+    GIVEN a Team model
+    WHEN a new Team is created
+    THEN check the team name, user id
+    """
+
+    team = Team(id=1, team_name="Black Kats", user_id=1)
+    assert team.team_name == "Black Kats"
+    assert team.user_id == 1
+
+def test_new_athlete():
+    """
+    GIVEN a Athlete Model
+    WHEN a new Athlete is created
+    THEN check the first name, last name, student number
+    """
+
+    athlete = Athlete(id=1, first_name="Leo", last_name="Mwenga", student_id=235467,
+                      team_id=1)
+    assert athlete.first_name == 'Leo'
+    assert athlete.last_name == 'Mwenga'
+    assert athlete.student_id == 235467
